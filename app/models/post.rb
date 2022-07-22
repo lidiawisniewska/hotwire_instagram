3b3174fb2ccf9b60c61cf3d9bf84130cd18dd388
Lidia Wisniewska <lidiawisnie@gmail.com>
2022-07-22 14:28:15 +0100

    Prepend new post to posts_list

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,11 @@ class Post < ApplicationRecord
 
   validates :body, presence: true
   validates :file, presence: true
+
+  after_create_commit do
+    broadcast_prepend_to "posts_list",
+      target: "posts",
+      partial: "posts/post",
+      locals: { post: self, user: nil }
+  end
 end
